68624eca9df6068ac470ad0eaf4cb0e2ec231d77
KavyashreeSK <kavyashree.sk@cloverbaytechnologies.com>
2025-06-10 21:09:51 +0530

    Main method overloading

---
+++ src/examples/Lab002.java
@@ -0,0 +1,19 @@
+package examples;
+
+public class Lab002 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hi");
+    }
+
+    public static void main()
+    {
+
+    }
+    public static void main(int abc) {
+
+        System.out.println("Hello");
+    }
+
+}
