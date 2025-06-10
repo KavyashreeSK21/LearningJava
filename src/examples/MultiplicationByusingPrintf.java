f2817db61bf55ae787f7cca9dd9a290cf31fa6b1
KavyashreeSK <kavyashree.sk@cloverbaytechnologies.com>
2025-06-10 20:47:17 +0530

    MultiplicationAssignment

---
+++ src/examples/MultiplicationByusingPrintf.java
@@ -0,0 +1,26 @@
+package examples;
+
+public class MultiplicationByusingPrintf {
+
+    public static void main(String[] args) {
+
+        int num=9;
+
+        //9 * 1 = 9
+        System.out.printf("%d * %d = %d%n",num,1,num * 1);
+
+        //9 * 2 = 18
+        System.out.printf("%d * %d = %d%n",num,2,num*2);
+        System.out.printf("%d * %d = %d%n",num,3,num*3);
+        System.out.printf("%d * %d = %d%n",num,4,num*4);
+        System.out.printf("%d * %d = %d%n",num,5,num*5);
+        System.out.printf("%d * %d = %d%n",num,6,num*6);
+        System.out.printf("%d * %d = %d%n",num,7,num*7);
+        System.out.printf("%d * %d = %d%n",num,8,num*8);
+        System.out.printf("%d * %d = %d%n",num,9,num*9);
+        System.out.printf("%d * %d = %d%n",num,10,num*10);
+
+
+
+    }
+}
---
+++ src/examples/Test.java
@@ -0,0 +1,13 @@
+package examples;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        int a=9;
+        int b=1;
+        int result = a*b;
+        System.out.printf("%d * %d = %d%n",a,b,result);
+
+    }
+}
